b265da90645763efbee91d0514b055e665d884bf
Fihkal <pihkalversion@gmail.com>
2018-06-04 22:52:54 +0900

    fix

--- server/init/routes.js
+++ server/init/routes.js
@@ -12,8 +12,7 @@ const reservedGuestsController = controllers && controllers.reservedGuests;
 
 export default (app) => {
   app.use(function(req, res, next) {
-    // res.header("Access-Control-Allow-Origin", "*");
-    res.header('Access-Control-Allow-Origin', 'http://localhost:8008');
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
